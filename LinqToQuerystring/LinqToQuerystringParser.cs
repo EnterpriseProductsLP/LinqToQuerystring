90eef7dcd53034376a909cc07ff7786cc9946da8
Eric Burcham <eburcham@eprod.com>
2020-01-13 12:48:12 -0600

    Finished upgrading to EF 6.4

--- LinqToQuerystring/LinqToQuerystringParser.cs
+++ LinqToQuerystring/LinqToQuerystringParser.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// $ANTLR 3.5.0.2 C:\\Users\\eburcham\\src\\OdataQueryToLinq\\LinqToQuerystring\\LinqToQuerystring.g 2020-01-13 12:22:19
+// $ANTLR 3.5.0.2 C:\\Users\\eburcham\\src\\OdataQueryToLinq\\LinqToQuerystring\\LinqToQuerystring.g 2020-01-13 12:43:13
 
 // The variable 'variable' is assigned but its value is never used.
 #pragma warning disable 219
